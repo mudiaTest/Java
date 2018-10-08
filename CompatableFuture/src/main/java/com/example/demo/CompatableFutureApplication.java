717f687b8bd39bb2ffb6c195bed832d14eef8860
lukasz <lukasz@biu.pl>
2018-10-08 10:04:36 +0200

    CompatableFuture

---
+++ CompatableFuture/src/main/java/com/example/demo/CompatableFutureApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompatableFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompatableFutureApplication.class, args);
+	}
+}
