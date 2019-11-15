3899e681df9b28523a53589761acdf19011f9257
mudia <mudia.pl@gmail.com>
2019-11-15 20:07:59 +0100

    porządki

---
+++ JavaBasicTest/src/main/java/ja1/JavaBasicTestApplication.java
@@ -0,0 +1,13 @@
+package ja1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBasicTestApplication.class, args);
+	}
+
+}
